2eca680072b56aad863d5d040c1f5c075514638d
miliberlin <michelle.liebheit@posteo.de>
2021-07-29 15:59:37 +0200

    Add button compontent and hero section

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,5 +1,22 @@
 import React from "react";
+import Button from "../Button";
 
 export default function Hero() {
-  return <section id="hero"></section>;
+  return (
+    <section id="hero" className="side-padding">
+      <div id="hero__info-box">
+        <h2>quis commodo odio aenean sed adipiscing diam donec adipiscing</h2>
+        <Button btnTxt="montes" />
+        <div id="awards">
+          <img src="/images/DKA.png" alt="" />
+          <img src="/images/trusted-shops-logo.png" alt="" />
+          <img src="/images/trust-logo.png" alt="" />
+          <div id="stars">
+            <img src="/images/stars.png" alt="" />
+            <p>4.1/5</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
