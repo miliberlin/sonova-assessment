b5ba1924405a8e8793bdb03548204a4c503df2cb
miliberlin <michelle.liebheit@posteo.de>
2021-07-29 15:10:13 +0200

    Add hero section with backrgound image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <Hero />
       </header>
     </div>
   );
